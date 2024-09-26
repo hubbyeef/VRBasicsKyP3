89617ddaff5fc211e0617dac33284419b620ae60
Jason Ky <744169@student.dusd.net>
2024-09-26 15:25:15 -0700

    Decorated the Room
    - Decorated the room
    - Added a teleportation area on the larrge mat

---
+++ VRRoomP3Ky/Assets/ClockScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    public GameObject secondHand;
+    public GameObject minuteHand;
+    public GameObject hourHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
