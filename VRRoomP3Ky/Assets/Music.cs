2d5a5025a1630be9312c62268db6ecb8be2355c1
Jason Ky <744169@student.dusd.net>
2024-10-25 11:41:29 -0700

    finished 1.4
    - Music stuff
    -Recorded

---
+++ VRRoomP3Ky/Assets/Music.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip laufey;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayMusic()
+    {
+        
+    }
+}
