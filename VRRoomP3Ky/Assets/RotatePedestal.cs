9f18bb9ef1745f428c95b2b6dc569dfde3f671c6
Jason Ky <744169@student.dusd.net>
2024-12-06 11:42:12 -0800

    finished challenge

--- VRRoomP3Ky/Assets/RotatePedestal.cs
+++ VRRoomP3Ky/Assets/RotatePedestal.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotatePedestal : MonoBehaviour
 {
     public GameObject pedestal;
+    public Slider slider;
     public float speed;
+    public float maxRotation = 180f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,6 @@ public class RotatePedestal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.Euler(0, (180 * slider.value), 0);
     }
 }
